c057bce3af3ba604bf7d81072170bea9a0f4c837
Bill Plein <260078+bplein@users.noreply.github.com>
2024-11-11 17:07:15 -0600

    Update rhel8-fa-iscsi-setup.sh

--- rhel8-fa-iscsi-setup.sh
+++ rhel8-fa-iscsi-setup.sh
@@ -38,6 +38,46 @@ dnf install iscsi-initiator-utils device-mapper device-mapper-multipath -y
 ##################
 
 cat << EOF > /etc/multipath.conf
+#!/usr/bin/env bash
+
+###### Setup iscsi (rhel7)
+
+##################
+# Create udev rules 
+# from example at support.purestorage.com
+# OVERWRITES THE FILE
+##################
+
+cat << EOF > /etc/udev/rules.d/99-pure-storage.rules
+# Recommended settings for Pure Storage FlashArray.
+# Use none scheduler for high-performance solid-state storage for SCSI devices
+ACTION=="add|change", KERNEL=="sd*[!0-9]", SUBSYSTEM=="block", ENV{ID_VENDOR}=="PURE", ATTR{queue/scheduler}="none"
+ACTION=="add|change", KERNEL=="dm-[0-9]*", SUBSYSTEM=="block", ENV{DM_NAME}=="3624a937*", ATTR{queue/scheduler}="none"
+
+# Reduce CPU overhead due to entropy collection
+ACTION=="add|change", KERNEL=="sd*[!0-9]", SUBSYSTEM=="block", ENV{ID_VENDOR}=="PURE", ATTR{queue/add_random}="0"
+ACTION=="add|change", KERNEL=="dm-[0-9]*", SUBSYSTEM=="block", ENV{DM_NAME}=="3624a937*", ATTR{queue/add_random}="0"
+
+# Spread CPU load by redirecting completions to originating CPU
+ACTION=="add|change", KERNEL=="sd*[!0-9]", SUBSYSTEM=="block", ENV{ID_VENDOR}=="PURE", ATTR{queue/rq_affinity}="2"
+ACTION=="add|change", KERNEL=="dm-[0-9]*", SUBSYSTEM=="block", ENV{DM_NAME}=="3624a937*", ATTR{queue/rq_affinity}="2"
+
+# Set the HBA timeout to 60 seconds
+ACTION=="add|change", KERNEL=="sd*[!0-9]", SUBSYSTEM=="block", ENV{ID_VENDOR}=="PURE", ATTR{device/timeout}="60"
+EOF
+
+##################
+# install pre-reqs
+##################
+
+dnf install iscsi-initiator-utils device-mapper device-mapper-multipath -y
+
+##################
+# create multipath.conf
+# OVERWRITES THE FILE
+##################
+
+cat << EOF > /etc/multipath.conf
 defaults {
         polling_interval       10
 }
@@ -72,6 +112,17 @@ devices {
         dev_loss_tmo             600
     }
 }
+
+EOF
+##################
+# load dm-multipath and start multipathd service
+modprobe -v dm-multipath
+systemctl start multipathd.service
+##################
+# enable and start iscsid service
+systemctl enable iscsid
+systemctl start iscsid
+##################
 
 EOF
 ##################
