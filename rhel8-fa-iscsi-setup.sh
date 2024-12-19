66b76e121e6eec295b3bc1fbb7427a148d12876f
Bill Plein <260078+bplein@users.noreply.github.com>
2024-12-19 11:37:29 -0600

    Update rhel8-fa-iscsi-setup.sh

--- rhel8-fa-iscsi-setup.sh
+++ rhel8-fa-iscsi-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-###### Setup iscsi (rhel7)
+###### Setup iscsi (rhel8)
 
 ##################
 # Create udev rules 
@@ -32,45 +32,6 @@ EOF
 
 dnf install iscsi-initiator-utils device-mapper device-mapper-multipath -y
 
-##################
-# create multipath.conf
-# OVERWRITES THE FILE
-##################
-
-cat << EOF > /etc/multipath.conf
-#!/usr/bin/env bash
-
-###### Setup iscsi (rhel7)
-
-##################
-# Create udev rules 
-# from example at support.purestorage.com
-# OVERWRITES THE FILE
-##################
-
-cat << EOF > /etc/udev/rules.d/99-pure-storage.rules
-# Recommended settings for Pure Storage FlashArray.
-# Use none scheduler for high-performance solid-state storage for SCSI devices
-ACTION=="add|change", KERNEL=="sd*[!0-9]", SUBSYSTEM=="block", ENV{ID_VENDOR}=="PURE", ATTR{queue/scheduler}="none"
-ACTION=="add|change", KERNEL=="dm-[0-9]*", SUBSYSTEM=="block", ENV{DM_NAME}=="3624a937*", ATTR{queue/scheduler}="none"
-
-# Reduce CPU overhead due to entropy collection
-ACTION=="add|change", KERNEL=="sd*[!0-9]", SUBSYSTEM=="block", ENV{ID_VENDOR}=="PURE", ATTR{queue/add_random}="0"
-ACTION=="add|change", KERNEL=="dm-[0-9]*", SUBSYSTEM=="block", ENV{DM_NAME}=="3624a937*", ATTR{queue/add_random}="0"
-
-# Spread CPU load by redirecting completions to originating CPU
-ACTION=="add|change", KERNEL=="sd*[!0-9]", SUBSYSTEM=="block", ENV{ID_VENDOR}=="PURE", ATTR{queue/rq_affinity}="2"
-ACTION=="add|change", KERNEL=="dm-[0-9]*", SUBSYSTEM=="block", ENV{DM_NAME}=="3624a937*", ATTR{queue/rq_affinity}="2"
-
-# Set the HBA timeout to 60 seconds
-ACTION=="add|change", KERNEL=="sd*[!0-9]", SUBSYSTEM=="block", ENV{ID_VENDOR}=="PURE", ATTR{device/timeout}="60"
-EOF
-
-##################
-# install pre-reqs
-##################
-
-dnf install iscsi-initiator-utils device-mapper device-mapper-multipath -y
 
 ##################
 # create multipath.conf
@@ -129,18 +90,6 @@ blacklist {
         product "Virtual disk"
       }
 }
-
-EOF
-##################
-# load dm-multipath and start multipathd service
-modprobe -v dm-multipath
-systemctl start multipathd.service
-##################
-# enable and start iscsid service
-systemctl enable iscsid
-systemctl start iscsid
-##################
-
 EOF
 ##################
 # load dm-multipath and start multipathd service
